33bce41f810c44da2002b4def2326b4823ae0f5a
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-10-03 16:47:52 -0700

    update

--- colorClass.cpp
+++ colorClass.cpp
@@ -15,24 +15,33 @@ Color() {
     blue = 335;
 }
 Color(int hue) {
-    blue = hue
+    red = hue;
+    blue = hue;
+    green = hue;
 
 }
-Color(int green) {red = 252;}
- //pararmeter constructor
- Color(int blue, int b);
+Color(int r, int g, int b) {
+     red = r;
+     blue = b;
+     green = g;
+
+}
+
 
  //setters and getters
- void setColor(int n )
+ void print() {
+ cout << "RGB: " << red << green << blue;
+ }
+
 };
 
 int main(){
-Color myColor;
-myColor.setRed(255);
-myColor.setBlue(256);
+Color myColor1;
+Color myColor2(245)
+
+Color myColor3.setBlue(256);
 myColor.setGreen(236);
 
-Color color_1;
 cout << color_1.getRedColor() << endl;
 myColor.print();
 
